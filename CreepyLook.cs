e4e67f896d62babfb03cabde8019034de00f8419
Joemx <jose.c.mondragon@gmail.com>
2020-04-22 16:08:34 -0500

    Update CreepyLook.cs
    two types of control

--- CreepyLook.cs
+++ CreepyLook.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CreepyLook : MonoBehaviour
 {
     public GameObject sphereToLook;
-
+    public bool controlBothAxis = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +19,20 @@ public class CreepyLook : MonoBehaviour
         pos.y = this.transform.position.y;
         this.transform.LookAt(pos);
 
-        float vert = Input.GetAxis("Vertical");
-        Vector3 myPos = this.transform.position;
-        myPos.z += vert;
-        this.transform.position = myPos;
-
+        if (controlBothAxis)
+        {
+            float vert = Input.GetAxis("Vertical");
+            float horz = Input.GetAxis("Horizontal");
+            Vector3 myPos = this.transform.position;
+            myPos = myPos + (new Vector3(horz, 0 , vert));
+            this.transform.position = myPos;
+        }
+        else {
+            float vert = Input.GetAxis("Vertical");
+            Vector3 myPos = this.transform.position;
+            Vector3 dir = this.transform.TransformDirection(Vector3.forward);
+            myPos = myPos + dir * vert * 0.5f;
+            this.transform.position = myPos;
+        }
     }
 }
