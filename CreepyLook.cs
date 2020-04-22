f2018a1add958df18baa8c949b04d0aeb13e36cc
Joemx <jose.c.mondragon@gmail.com>
2020-04-22 16:27:13 -0500

    new moves
    implemented moves and material

--- CreepyLook.cs
+++ CreepyLook.cs
@@ -6,10 +6,13 @@ public class CreepyLook : MonoBehaviour
 {
     public GameObject sphereToLook;
     public bool controlBothAxis = false;
+    Vector3 cameraOffset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 pos = this.transform.position;
+        Vector3 camPos = Camera.main.transform.position;
+        cameraOffset = camPos - pos;
     }
 
     // Update is called once per frame
@@ -24,15 +27,17 @@ public class CreepyLook : MonoBehaviour
             float vert = Input.GetAxis("Vertical");
             float horz = Input.GetAxis("Horizontal");
             Vector3 myPos = this.transform.position;
-            myPos = myPos + (new Vector3(horz, 0 , vert));
+            myPos = myPos + (new Vector3(horz, 0, vert)) * 0.1f;
             this.transform.position = myPos;
         }
         else {
             float vert = Input.GetAxis("Vertical");
             Vector3 myPos = this.transform.position;
             Vector3 dir = this.transform.TransformDirection(Vector3.forward);
-            myPos = myPos + dir * vert * 0.5f;
+            myPos = myPos + dir * vert * 0.1f;
             this.transform.position = myPos;
         }
+
+        Camera.main.transform.position = this.transform.position + cameraOffset;
     }
 }
